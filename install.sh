a56b5a6de7222cdb7be8c69865383788d59dac19
Johnny4251 <JohnAngus425@gmail.com>
2025-02-19 08:59:28 -0500

    Setup python venv

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo apt update
+
+echo "Setting up python venv"
+python3 -m venv venv
+source venv/bin/activate
+pip install opencv-python
+deactivate
+
 echo "Making KISSZYB"
 cd kisszyb
 make
